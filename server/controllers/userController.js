29d6431cdf570f4905465be4f7c59b98c97f21bf
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2025-03-10 21:05:42 +0700

    api register and login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,11 +6,32 @@ const register = asyncHandler(async (req, res) => {
   if (!firstName || !lastName || !email || !mobile || !password) {
     return res.status(400).json({ success: false, message: "Missing fields" });
   }
-  const response = await User.create(req.body);
-  return res.status(200).json({
-    success: response ? true : false,
-    response,
-  });
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Error("User already exists");
+  } else {
+    const newUser = await User.create(req.body);
+    return res
+      .status(200)
+      .json({ success: newUser ? true : false, message: newUser ? "User created" : "User not created" });
+  }
 });
 
-module.exports = { register };
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ success: false, message: "Missing fields" });
+  }
+  const response = await User.findOne({ email });
+  if (response && (await response.isPasswordMatched(password))) {
+    const { password, role, ...userData } = response.toObject();
+    return res.status(200).json({
+      success: true,
+      userData,
+    });
+  } else {
+    throw new Error("Invalid credential");
+  }
+});
+
+module.exports = { register, login };
--- server/models/user.js
+++ server/models/user.js
@@ -68,4 +68,10 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+userSchema.methods = {
+  isPasswordMatched: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
+};
+
 module.exports = mongoose.model("User", userSchema);
